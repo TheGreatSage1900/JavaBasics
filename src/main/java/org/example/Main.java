b73da9762b52340d21c234be6a0e1f26c04d2593
TheGreatSage <thegreatsage1900@gmail.com>
2025-03-27 08:50:19 +0530

    Switch condition.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -22,12 +23,42 @@ public class Main {
 
         int[] arr1 = new int[10];
         int[] arr2 = new int[]{1,2,3,4,5};
-
         arr1[0] = 5;
         arr2[0] = 10;
 
-        int[] arr = IntStream.range(1,5).toArray();
-        System.out.println(Arrays.toString(arr));
+        // Ternary Operator should return something
+        int min = 0; int max = 7;
+        int randomNum = new Random().nextInt(max - min + 1) + min;
+        String ternaryResult = (randomNum % 2 == 0)?"Even":"odd";
+
+        // switch
+
+        switch (new Random().nextInt(max - min + 1)+min){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4,5,6 -> System.out.println("unknown");
+            default -> System.out.println("Still unknown");
+        }
+
+        // switch with returns
+
+        String res = switch("String"){
+            case "Data"-> "Data";
+            default -> "nothing";
+        };
+
+        // switch with pattern matching
+
+        Object obj = "String";
+        switch(obj){
+            case String s -> System.out.println("String");
+            case Integer i -> System.out.println("Integer");
+            default -> System.out.println("Default");
+        }
+
+
+
 
     }
 }
