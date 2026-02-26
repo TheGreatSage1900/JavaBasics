ffce66da028ebda3c2b27d6570ee1916d41524da
TheGreatSage <thegreatsage@TheGreatSages-MacBook-Air.local>
2026-02-26 23:24:39 +0530

    adding data types

---
+++ src/main/java/org/example/concepts/DataTypes.java
@@ -0,0 +1,25 @@
+package org.example.concepts;
+
+/*
+This class contains frequently used data types and idiomatic data types used in several use cases.
+*/
+
+public class DataTypes {
+
+    // Integer related data types ( Wrapper class )
+    Integer integer = 55; // 4 byte
+    Long longType = 55L; //
+    Double doubleType = 55D;
+    Float floatType = 44F;
+
+    // Integer related data types ( primitive types )
+    int primitiveInt = 55;
+    long primitiveLong = 55L;
+    double primitiveDouble = 55d;
+    float primitiveFloat = 55f;
+
+
+    static void main() {
+
+    }
+}
